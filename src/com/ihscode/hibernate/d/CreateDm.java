a2450f38992f3ccedc299e50b8669be5a0cb78d5
Admin i <Admin i@DESKTOP-F5BCCST>
2021-06-28 15:01:38 +0530

    associate the objects

--- src/com/ihscode/hibernate/d/CreateDm.java
+++ src/com/ihscode/hibernate/d/CreateDm.java
@@ -4,34 +4,44 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.ihscode.hibernate.d.entity.Instructor;
+import com.ihscode.hibernate.d.entity.InstructorDetail;
 import com.ihscode.hibernate.d.entity.Student;
 
-public class CreateStudentDm {
+public class CreateDm {
 
 	public static void main(String[] args) {
 
 		// create session factory
 		SessionFactory factory = new Configuration()
 								.configure("hibernate.cfg.xml")
-								.addAnnotatedClass(Student.class)
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
 								.buildSessionFactory();
 		
 		// create session
 		Session session  = factory.getCurrentSession();
 		
 		try {
-			// use the session object to save Java object
+
+			// create the objects
+			Instructor tempInstructor =
+					new Instructor("Ihs", "Kla", "ihus@luv2code.com");
 			
-			// create a student object
-			System.out.println("Creating a new stduent object...");
-			Student tempStudent = new Student("Ihu", "Bhai", "ihuscoding.com");
+			InstructorDetail tempinstructorDetail =
+					new InstructorDetail(
+							"http://www.ihs2code.com/youtube",
+							"Luv 2 code!!!!!!");
+			
+			// associate the objects
+			tempInstructor.setInstructorDetail(tempinstructorDetail);
+			
 			
 			// start a transaction
 			session.beginTransaction();
 			
-			// save the student object
-			System.out.println("Saving the student...");
-			session.save(tempStudent);
+			// save the instructor
+			
 			
 			// commit transaction
 			session.getTransaction().commit();
