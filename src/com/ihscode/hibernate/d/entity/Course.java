98b6a434bb04fa1b2aa42753d563eea9d9985abd
Admin i <ihsanmpm@gmail.com>
2021-07-02 21:46:03 +0530

    .

--- src/com/ihscode/hibernate/d/entity/Course.java
+++ src/com/ihscode/hibernate/d/entity/Course.java
@@ -1,15 +1,18 @@
 package com.ihscode.hibernate.d.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -39,6 +42,8 @@ public class Course {
 	@JoinColumn(name="Instructor_id") 
 	private Instructor instructor;
 	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="course_id")
 	private List<Review> reviews;
 	
 	public Course() {
@@ -73,7 +78,6 @@ public class Course {
 		this.instructor = instructor;
 	}
 	
-	
 
 	public List<Review> getReviews() {
 		return reviews;
@@ -81,6 +85,17 @@ public class Course {
 
 	public void setReviews(List<Review> reviews) {
 		this.reviews = reviews;
+	}
+	
+	// add a convenience method
+	
+	public void addReview(Review theReview) {
+		
+		if(reviews == null) {
+			reviews = new ArrayList<>();
+		}
+		
+		reviews.add(theReview);
 	}
 
 	@Override
